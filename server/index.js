a58814b217e9ee2272c66973a83699698eb38825
Brian Chen <brianchen8990@gmail.com>
2018-06-30 23:31:16 +0200

    Add scripts and schedules json with query resolvers

--- server/index.js
+++ server/index.js
@@ -1,31 +1,81 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql, UserInputError } = require("apollo-server");
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
+const array2List = (arr, key) => {
+  const result = {};
+  arr.forEach(e => {
+    if (e[key]) {
+      result[e[key]] = e;
+    }
+  });
+  return result;
+};
+
 const songs = require("./songs.json");
+const songList = array2List(songs, "id");
+const scripts = require("./scripts.json");
+const scriptList = array2List(scripts, "id");
+const schedules = require("./schedules.json");
+const scheduleList = array2List(schedules, "date");
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
   type Page {
     id: ID!
-    lines: [String]!
+    lines: [String!]!
   }
 
-  # This "Song" type can be used in other type declarations.
-  type Song {
+  interface LibraryEntry {
+    id: ID!
+    pages: [Page!]!
+  }
+
+  type Song implements LibraryEntry {
+    id: ID!
     name: String!
     lang: String!
-    pages: [Page]!
+    pages: [Page!]!
   }
 
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
+  type Scripts implements LibraryEntry {
+    id: ID!
+    name: String!
+    pages: [Page!]!
+  }
+
+  enum RefType {
+    SONG
+    SCRIPTS
+  }
+
+  type ReferredEntry {
+    id: ID!
+    ref: ID!
+    refType: RefType!
+  }
+
+  union ScheduleEntry = ReferredEntry | Page
+
+  type Schedule {
+    id: ID!
+    date: String!
+    entries: [ScheduleEntry!]!
+  }
+
+  type PageResult {
+    ref: ID
+    refType: RefType
+    lines: [String!]!
+  }
+
+  type ScheduleResult {
+    id: ID!
+    pages: [PageResult!]!
+  }
+
   type Query {
-    songs: [Song]!
+    songs: [Song!]!
+    scripts: [Scripts!]!
+    schedules: [Schedule!]!
+    getScheduleByDate(date: String!): ScheduleResult
   }
 `;
 
@@ -33,7 +83,59 @@ const typeDefs = gql`
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    songs: () => songs
+    songs: () => songs,
+    scripts: () => scripts,
+    schedules: () => schedules,
+    getScheduleByDate: (_, { date }) => {
+      const schedule = scheduleList[date];
+
+      if (schedule) {
+        const pages = [];
+        schedule.entries.forEach(e => {
+          if (e.ref !== undefined && e.refType !== undefined) {
+            switch (e.refType) {
+              case 0:
+                const song = songList[e.ref];
+                if (song) {
+                  pages.push(
+                    ...song.pages.map(sp => ({
+                      ref: e.ref,
+                      refType: e.refType,
+                      lines: sp.lines
+                    }))
+                  );
+                }
+                break;
+              case 1:
+                const script = scriptList[e.ref];
+                if (script) {
+                  pages.push(
+                    ...script.pages.map(sp => ({
+                      ref: e.ref,
+                      refType: e.refType,
+                      lines: sp.lines
+                    }))
+                  );
+                }
+                break;
+            }
+          } else {
+            pages.push(e);
+          }
+        });
+
+        if (pages.length === 0) {
+          return new UserInputError("Schedule is empty");
+        }
+
+        return {
+          id: schedule.id,
+          pages
+        };
+      }
+
+      return new UserInputError("No schedule found for " + date);
+    }
   }
 };
 
