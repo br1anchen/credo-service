7301924c28a17e35c579f850c9ceb6310e0d4d7b
Brian Chen <brianchen8990@gmail.com>
2018-08-04 13:00:33 +0200

    Add updateLytrics mutation

--- server/graphql/MutationType.js
+++ server/graphql/MutationType.js
@@ -4,6 +4,7 @@ const Song = require('../models/Song');
 const MutationType = gql`
   type Mutation {
     addSong(name: String!, lang: String!, lyrics: [String!]!): Song
+    updateLyrics(name: String!, lang: String!, lyrics: [String!]!): Song
   }
 `;
 
@@ -12,6 +13,9 @@ const MutationResolver = {
     addSong: (_, { name, lang, lyrics }) => {
       const song = new Song({ name, lang, lyrics });
       return song.save();
+    },
+    updateLyrics: (_, { name, lang, lyrics }) => {
+      return Song.where({ name, lang }).findOneAndUpdate({ lyrics });
     }
   }
 };
